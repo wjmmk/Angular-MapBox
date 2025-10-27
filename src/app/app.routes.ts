c938c71f3bf944289bb82af34c7d16b9ba9e3b63
William Mosquera <wjmmk3@gmail.com>
2025-10-27 14:33:27 -0500

    feat: Initial Setup.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
+import { HousesPages } from './pages/houses-pages/houses-pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullscreenMapPage,
+    title: 'Vista completa'
+  },
+  {
+    path: 'markers',
+    component: MarkersPage,
+    title: 'Marcadores'
+  },
+  {
+    path: 'houses',
+    component: HousesPages,
+    title: 'Propiedades disponibles'
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen'
+  },
+];
